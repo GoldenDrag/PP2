c7deea22a73a55e0da40231ea2eacfbc54213db0
GoldenDrag <mr.ablaysan@gmail.com>
2019-02-10 14:29:31 +0600

    one

---
+++ week1/Task4/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            int n = int.Parse(str);  //number of strings
+
+            for (int i = 0; i <= n; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    Console.Write("[*]");  //full this strings
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
