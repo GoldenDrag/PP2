5f37df93906ddb8e4bc5ba682b7e2165e47099ab
GoldenDrag <mr.ablaysan@gmail.com>
2019-02-13 22:15:20 +0600

    Based on example

---
+++ week2/Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+        }
+
+        static void FolderOpener(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                FolderOpener(d, level + 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Технодом\Documents\PP2\week2\Task3\gnome");
+            FolderOpener(d, 1);
+        }
+    }
+}
