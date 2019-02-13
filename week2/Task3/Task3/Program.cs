8e30069b1e476d2a3cd0ac30b78cdc1193e94cd1
GoldenDrag <mr.ablaysan@gmail.com>
2019-02-13 22:22:39 +0600

    I got it

--- week2/Task3/Task3/Program.cs
+++ week2/Task3/Task3/Program.cs
@@ -9,24 +9,24 @@ namespace Task3
 {
     class Program
     {
-        static void PrintSpaces(int level)
+        static void PrintSpaces(int level)  //function to make a spaces
         {
             for (int i = 0; i < level; i++)
                 Console.Write("  ");
         }
 
-        static void FolderOpener(DirectoryInfo directory, int level)
+        static void FolderOpener(DirectoryInfo directory, int level)  //function to open every folder recusively
         {
-            FileInfo[] files = directory.GetFiles();
+            FileInfo[] files = directory.GetFiles();   //get files in folder
             DirectoryInfo[] directories = directory.GetDirectories();
 
-            foreach (FileInfo file in files)
+            foreach (FileInfo file in files)  //using function put spaces back to file names to show where is it contained
             {
                 PrintSpaces(level);
                 Console.WriteLine(file.Name);
             }
 
-            foreach (DirectoryInfo d in directories)
+            foreach (DirectoryInfo d in directories)  //same thing with a folder and opening it immidietly
             {
                 PrintSpaces(level);
                 Console.WriteLine(d.Name);
@@ -36,7 +36,7 @@ namespace Task3
 
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Технодом\Documents\PP2\week2\Task3\gnome");
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Технодом\Documents\PP2\week2\Task3\gnome");  //path of top folder
             FolderOpener(d, 1);
         }
     }
